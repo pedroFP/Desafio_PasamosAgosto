3142e7fae06121d0ab30b5caa1f75ea4351231ea
pedro.araya.labra@gmail.com <pedro.araya.labra@gmail.com>
2018-08-31 22:15:10 -0300

    ADD event scaffold

---
+++ abuelosApp/db/migrate/20180901011428_create_events.rb
@@ -0,0 +1,13 @@
+class CreateEvents < ActiveRecord::Migration[5.2]
+  def change
+    create_table :events do |t|
+      t.string :name
+      t.string :title
+      t.string :category
+      t.date :date
+      t.integer :phone
+
+      t.timestamps
+    end
+  end
+end
